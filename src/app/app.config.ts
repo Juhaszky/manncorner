b8608b69cbad2bafd59dfaa8917e3ded06effe1a
Juhaszky <juhasz.krisztian2000@gmail.com>
2024-03-24 21:54:12 +0100

    refactor(main container): display container refactor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,12 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+  ],
 };
