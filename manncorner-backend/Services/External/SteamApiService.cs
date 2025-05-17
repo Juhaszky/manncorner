758a2e9157877df7d01e1697af3175b73d3ca04f
Juhaszky <juhasz.krisztian2000@gmail.com>
2025-05-17 20:47:17 +0200

    feat(steamApiService): mock date added to not to call steam apis

--- manncorner-backend/Services/External/SteamApiService.cs
+++ manncorner-backend/Services/External/SteamApiService.cs
@@ -14,7 +14,30 @@ public class SteamApiService : ISteamApiService
         {
             var url = $"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={_apiKey}&steamids={steamId}";
             var response = await _httpClient.GetAsync(url);
-
+            var mockResponse = @"{
+            ""response"": {
+                ""players"": [
+                    {
+                        ""steamid"": ""76561198027857565"",
+                        ""communityvisibilitystate"": 3,
+                        ""profilestate"": 1,
+                        ""personaname"": ""Juhaszky"",
+                        ""commentpermission"": 1,
+                        ""profileurl"": ""https://steamcommunity.com/id/Juhaszky/"",
+                        ""avatar"": ""https://avatars.steamstatic.com/f6826be3e9442a9352849013a5f84f8c6c5b09dd.jpg"",
+                        ""avatarmedium"": ""https://avatars.steamstatic.com/f6826be3e9442a9352849013a5f84f8c6c5b09dd_medium.jpg"",
+                        ""avatarfull"": ""https://avatars.steamstatic.com/f6826be3e9442a9352849013a5f84f8c6c5b09dd_full.jpg"",
+                        ""avatarhash"": ""f6826be3e9442a9352849013a5f84f8c6c5b09dd"",
+                        ""lastlogoff"": 1747342985,
+                        ""personastate"": 0,
+                        ""primaryclanid"": ""103582791474593353"",
+                        ""timecreated"": 1279572634,
+                        ""personastateflags"": 0
+                    }
+                ]
+            }
+        }";
+        return mockResponse;
             if (response.IsSuccessStatusCode)
             {
                 return await response.Content.ReadAsStringAsync();
